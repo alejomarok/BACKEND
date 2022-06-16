8fc1d03429436f793945c8006a0c1cc30dad3674
Alejo Marcó <89427662+alejomarok@users.noreply.github.com>
2022-06-15 22:59:42 -0300

    Add files via upload

---
+++ DESAFIO4/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+
+const app = express()
+
+const puerto = 8080
+
+const router = require('./app/routes/index')
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(`${__dirname}/public`))
+app.use(router)
+
+
+app.listen(puerto, err => {
+	if (err) {
+		console.log(`Se produjo un error al iniciar el servidor ${puerto}`)
+	} else {
+		console.log(`El servidor esta escuchando el puerto ${puerto}`)
+	}
+})
