de3e3dd3cb8ce983dd368b4d5a219d2a36fe01ae
Alejo Marcó <89427662+alejomarok@users.noreply.github.com>
2022-06-29 23:07:00 -0300

    Add files via upload

---
+++ DESAFIO6/parte3/routes/index.js
@@ -0,0 +1,11 @@
+const { Router } = require('express')
+const router = Router()
+const champs = [{ name: 'alejo', lastName: 'marco' },{ name: 'elias', lastName: 'rivarola' },{ name: 'maxi', lastName: 'bergamin' }, ]
+
+router.get('/', (req, res) => {
+    res.render('champs', { champs, hasAny: true} )
+})
+
+
+
+module.exports = router 
